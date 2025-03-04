d2ea26c1ab84a7258abc3b8d99203485d30f50a1
linhmon <nguyenlinhmon@gmail.com>
2025-03-04 20:52:47 +0900

    add qualifications

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Home from "@/components/home/home";
 import About from "@/components/about/about";
 import Skill from "@/components/skills/Skill";
 import Services from "@/components/services/Services";
+import Qualification from "@/components/qualifacation/Qualification";
 
 export default function index() {
   return (
@@ -13,6 +14,7 @@ export default function index() {
         <About />
         <Skill />
         <Services />
+        <Qualification />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
